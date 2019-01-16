647cc04d4316a7f07cd9928dc010c2a80293e84e
FORNALI Damien <itsdamoy@gmail.com>
2019-01-16 09:31:48 +0100

    Basis

---
+++ internal/code/src/com/drago/arduinoml/internal/ArduinoML.java
@@ -0,0 +1,13 @@
+package com.drago.arduinoml.internal;
+import java.io.File;
+
+public class ArduinoML {
+	public static void main(String[] args) {
+//		GroovuinoMLDSL dsl = new GroovuinoMLDSL();
+//		if(args.length > 0) {
+//			dsl.eval(new File(args[0]));
+//		} else {
+//			System.out.println("/!\\ Missing arg: Please specify the path to a Groovy script file to execute");
+//		}
+	}
+}
