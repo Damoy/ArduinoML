3b26c033da814fc0b215d113c67da5612f8538cc
FORNALI Damien <itsdamoy@gmail.com>
2019-01-22 10:19:49 +0100

    Simple alarm scenario

---
+++ embedded/code/scripts/groovy/SimpleAlarm.groovy
@@ -0,0 +1,13 @@
+sensor "button" onPin 16
+actuator "led" onPin 17
+actuator "buzzer" onPin 18
+
+state "on" means "led" becomes "high" and "buzzer" becomes "high"
+state "off" means "led" becomes "low" and "buzzer" becomes "low"
+
+initial "off"
+
+from "on" to "off" when "button" becomes "high"
+from "off" to "on" when "button" becomes "high"
+
+export "SimpleAlarm"
