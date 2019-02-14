1d7fb8e59227a2689ab60d6ed8a070e077c11f71
Duminy Gaétan <gaetan.duminy@gmail.com>
2019-02-14 18:41:51 +0100

    MAJ time

--- plot_external/plot_arduino.py
+++ plot_external/plot_arduino.py
@@ -2,19 +2,28 @@ from serial import Serial
 import numpy
 import matplotlib.pyplot as plt
 from drawnow import *
+import datetime
 
 plt.ion()
 cnt=0
 compt = []
 y = []
+times = []
 modes = []
 states = []
 plt.ylabel('State')
-plt.title('Histogram of IQ')
 state = 0
 titre = ""
 actual_mode = 0
 new_state = False
+
+def setTimes(start, times):
+    now = datetime.datetime.now()
+    time = now - start
+    s = time.seconds
+    minutes = s // 60
+    seconds = s - (minutes * 60)
+    times.append('{:02}:{:02}'.format(int(minutes), int(seconds)))
 
 def getArduinoLogs(port_serie) :
     arduinoString = str(port_serie.readline())[2:-5]
@@ -29,11 +38,13 @@ def custom_plot():
     if (len(states) > 0):
         plt.axis((max(0, x_size - 20), max(20, x_size), 0, len(states[actual_mode])))
         plt.yticks(numpy.arange(len(states[actual_mode])),  states[actual_mode])
+        plt.xticks(compt[-20:], times[-20:], rotation=45)
 
 with Serial(port="COM5", baudrate=9600, timeout=1, writeTimeout=1) as port_serie:
     while (port_serie.inWaiting()==0):
         pass
     if port_serie.isOpen():
+        start = datetime.datetime.now()
         while True:
             dataArray = getArduinoLogs(port_serie)
             try:
@@ -55,6 +66,9 @@ with Serial(port="COM5", baudrate=9600, timeout=1, writeTimeout=1) as port_serie
                         elif  (subData[0] == "mode") :
                             titre = modes[int(subData[1])]
                             actual_mode = int(subData[1])
+                            compt = []
+                            y = []
+                            times = []
                             print(titre)
 
                         elif (subData[0] == "state") :
@@ -63,6 +77,8 @@ with Serial(port="COM5", baudrate=9600, timeout=1, writeTimeout=1) as port_serie
                             state = subData[1]
                             y.append(state)
                             cnt=cnt+1
+                            setTimes(start, times)
+
                 
             except IndexError:
                 print("Mauvais lecture")
@@ -71,6 +87,7 @@ with Serial(port="COM5", baudrate=9600, timeout=1, writeTimeout=1) as port_serie
                 compt.append(str(cnt))
                 y.append(state)
                 cnt=cnt+1
+                setTimes(start, times)
 
             drawnow(custom_plot)
             new_state = False
